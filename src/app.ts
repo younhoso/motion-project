49c74a9bc82e061abd74775c16cb7351e9ebfdb8
younhoso <thdbsgh3443@kakao.com>
2024-08-14 16:09:24 +0900

    Feat: 페이지 아이템 컨테이너 만들기

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
+import { Component } from "./components/components.js";
 import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { VideoComponent } from "./components/page/item/video.js";
-import { PageComponent } from "./components/page/page.js";
+import { Composable, PageComponent } from "./components/page/page.js";
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(root: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(root);
@@ -14,19 +15,19 @@ class App {
       "Image Title",
       "https://picsum.photos/600/300"
     );
-    image.attachTo(root, "beforeend");
+    this.page.addChild(image);
 
     const video = new VideoComponent(
       "Video Title",
       "https://www.youtube.com/watch?v=K3-jG52XwuQ"
     );
-    video.attachTo(root, "beforeend");
+    this.page.addChild(video);
 
     const note = new NoteComponent("Note Title", "Note Body");
-    note.attachTo(root, "beforeend");
+    this.page.addChild(note);
 
     const todo = new TodoComponent("Todo Title", "Todo Item");
-    todo.attachTo(root, "beforeend");
+    this.page.addChild(todo);
   }
 }
 
