15615c1f111e93f8e460faa0d6c914e30e7e4196
younhoso <thdbsgh3443@kakao.com>
2024-08-15 15:08:53 +0900

    Feat: remove 삭제 기능 작업

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -4,10 +4,14 @@ export interface Composable {
   addChild(child: Component): void;
 }
 
+type OnCloseListener = () => void;
+
 class PageItemComponent
   extends BaseComponent<HTMLElement>
   implements Composable
 {
+  private closeListener?: OnCloseListener;
+
   constructor() {
     super(`<li class="page-item">
       <section class="page-item__body"></section>
@@ -15,6 +19,11 @@ class PageItemComponent
         <button class="close">&times;</button>
       </div>
     </li>`);
+
+    const closeBtn = this.element.querySelector(".close")! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
   }
 
   addChild(child: Component) {
@@ -22,6 +31,10 @@ class PageItemComponent
       ".page-item__body"
     )! as HTMLElement;
     child.attachTo(container);
+  }
+
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
   }
 }
 
@@ -37,5 +50,8 @@ export class PageComponent
     const item = new PageItemComponent();
     item.addChild(section);
     item.attachTo(this.element, "beforeend");
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    });
   }
 }
